edbce8fbbf05d308f85ab5ce8f28420f56f407d1
Ryan Willis <ryan_h_willis@hotmail.com>
2023-12-11 10:21:12 +0000

    Initial Commit

---
+++ LayoutSwiftUI/LayoutSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutSwiftUIApp.swift
+//  LayoutSwiftUI
+//
+//  Created by Ryan Hayd'n Willis on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
